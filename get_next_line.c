e7fa4bf066a512246a31f7b71c97bb425b224b2d
RJ Hallsted <rhallste@student.42.us.org>
2017-10-13 18:32:15 -0700

    Works sort of

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,49 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 16:38:28 by rhallste          #+#    #+#             */
-/*   Updated: 2017/10/09 19:14:36 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/10/13 18:31:13 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <string.h>
 #include "libft/libft.h"
 #include "get_next_line.h"
 
 int	get_next_line(const int fd, char **line)
 {
-	char	buff[BUFF_SIZE];
+	int		index;
 	int		rv;
-	int		bytes_read;
-	char	*line;
-	
-	
+	int		read_line;
+	size_t	line_size;
+	char	buff[BUFF_SIZE];
+
+	line_size = 0;
+	index = 0;
+	*line = NULL;
+	ft_bzero(buff, BUFF_SIZE);
+	rv = 1;
+	read_line = 0;
+	while (rv != 0 && !read_line)
+	{
+		while (index < BUFF_SIZE - 1 && (rv = read(fd, buff + index, 1) > 0)
+				&& buff[index] != '\n')
+			index++;
+		if (buff[index] == '\n' || rv == 0)
+		{
+			index += (buff[index] == '\n') ? 0 : 1;
+			buff[index] = '\0';
+			read_line = 1;
+		}
+		else
+		{
+			buff[index] = '\0';
+			index = 0;
+		}
+		line_size += index;
+		*line = ft_realloc(*line, line_size);
+		ft_strcat(*line, buff);
+		ft_bzero(buff, BUFF_SIZE);
+	}
+	return ((read_line) ? !(rv == 0) : -1);
 }
--- test.c
+++ test.c
@@ -1,31 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/09 19:22:06 by rhallste          #+#    #+#             */
-/*   Updated: 2017/10/13 18:29:28 by rhallste         ###   ########.fr       */
+/*   Created: 2017/10/13 17:51:48 by rhallste          #+#    #+#             */
+/*   Updated: 2017/10/13 18:01:53 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "libft.h"
-
+#include <fcntl.h>
 #include <stdio.h>
+#include "get_next_line.h"
 
-void	*ft_realloc(void *src, size_t size)
+int	main(int argc, char **argv)
 {
-	void *new;
+	int fd;
+	int i;
+	char *line;
 
-	if (!src)
-		return (ft_memalloc(size));
-	if (!size)
-		return (NULL);
-	if (!(new = ft_memalloc(size)))
-		return (NULL);
-	ft_memcpy(new, src, size);
-	free(src);
-	return (new);
+	if (argc == 2)
+	{
+		if((fd = open(argv[1], O_RDONLY)) == -1)
+			return (0);
+		i = 0;
+		printf("Run's correctly\n");
+		while (get_next_line(fd, &line) > 0)
+		{
+			printf("Line %d: ", i++);
+			printf("%s\n", line);
+		}
+	}
+	return (1);
 }
