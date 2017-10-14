f6407ad492177a80482d61012b93d95513b24c2b
RJ Hallsted <rhallste@student.42.us.org>
2017-10-13 18:41:25 -0700

    Works with any size buffer now

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 16:38:28 by rhallste          #+#    #+#             */
-/*   Updated: 2017/10/13 18:31:13 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/10/13 18:36:50 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,20 @@ int	get_next_line(const int fd, char **line)
 	read_line = 0;
 	while (rv != 0 && !read_line)
 	{
-		while (index < BUFF_SIZE - 1 && (rv = read(fd, buff + index, 1) > 0)
+		while (index < BUFF_SIZE && (rv = read(fd, buff + index, 1) > 0)
 				&& buff[index] != '\n')
 			index++;
 		if (buff[index] == '\n' || rv == 0)
 		{
-			index += (buff[index] == '\n') ? 0 : 1;
-			buff[index] = '\0';
+			if (buff[index] == '\n')
+				buff[index] = '\0';
 			read_line = 1;
-		}
-		else
-		{
-			buff[index] = '\0';
-			index = 0;
 		}
 		line_size += index;
 		*line = ft_realloc(*line, line_size);
-		ft_strcat(*line, buff);
+		ft_strncat(*line, buff,	MIN(index, BUFF_SIZE));
 		ft_bzero(buff, BUFF_SIZE);
+		index = 0;
 	}
 	return ((read_line) ? !(rv == 0) : -1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 16:40:51 by rhallste          #+#    #+#             */
-/*   Updated: 2017/10/09 16:42:38 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/10/13 18:40:50 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFF_SIZE
-#  define BUFF_SIZE	100
+#  define BUFF_SIZE	3
 # endif
 
 int	get_next_line(const int fd, char **line);
