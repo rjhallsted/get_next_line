7d82df78f071cb449ebf8cf29ef4e7b17e3b7e7f
RJ Hallsted <rhallste@student.42.us.org>
2017-10-18 23:15:28 -0700

    Switched to using a static struct

--- get_next_line.c
+++ get_next_line.c
@@ -6,95 +6,85 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 16:38:28 by rhallste          #+#    #+#             */
-/*   Updated: 2017/10/18 17:13:14 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/10/18 23:14:12 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-//fix segfault for if line is passed as NULL
 
 #include <unistd.h>
 #include <string.h>
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-static void	shift_chars_left(char *buff, int shift_by)
+/*
+** shift_chars_left assumes shift_by < BUFF_SIZE
+*/
+
+static void	shift_chars_left(t_data *data, int shift_by)
 {
 	int i;
 
 	i = 0;
-	while (buff[i] && buff[i + shift_by])
+	while (data->buff[i] && data->buff[i + shift_by])
 	{
-		buff[i] = buff[i + shift_by];
+		data->buff[i] = data->buff[i + shift_by];
 		i++;
 	}
-	ft_bzero(buff + i, BUFF_SIZE - i);
+	ft_bzero(data->buff + i, BUFF_SIZE - i);
 }
 
-static void	buff_to_line(char **line, char *buff, size_t line_len, int copy_len)
+static void	buff_to_line(char **line, t_data *data, int copy_len)
 {
 	int shift_by;
 
-	*line = ft_realloc(*line, line_len + 1);
-	ft_strncat(*line, buff, copy_len);
+	*line = ft_realloc(*line, data->line_len + 1);
+	ft_strncat(*line, data->buff, copy_len);
 	shift_by = (copy_len < BUFF_SIZE) ? copy_len + 1 : 0;
-	shift_chars_left(buff, shift_by);
+	shift_chars_left(data, shift_by);
 }
 
-static int	prelim_checks(char **line, char *buff)
+static int	prelim_checks(char **line, t_data *data)
 {
 	char	*char_pos;
-	int		line_length;
 
 	if (line)
 		ft_strclr(*line);
-	char_pos = ft_strchr(buff, '\n');
-	line_length = (char_pos) ? (char_pos - buff) : ft_strlen(buff);
-	buff_to_line(line, buff, line_length, line_length);
-	if (char_pos)
-		return (-1);
-	return (line_length);
+	char_pos = ft_strchr(data->buff, '\n');
+	data->line_len = (char_pos) ? (char_pos - data->buff) : ft_strlen(data->buff);
+	buff_to_line(line, data, data->line_len);
+	return ((char_pos != NULL));
 }
 
-/*
-** run_copy normally returns the new line_length
-** If done reading line, return (-1);
-** ^^Done because the normal return value needs to be
-** ^^positive, so we can't use normal success codes
-*/
-
-static int	run_copy(char **line, char *buff, int line_len, int rv)
+static int	run_copy(char **line, t_data *data, int rv)
 {
 	char	*char_pos;
 	int		copy_len;
 
-	char_pos = ft_strchr(buff, '\n');
-	copy_len = (char_pos) ? (char_pos - buff) : rv;
-	line_len += copy_len;
-	buff_to_line(line, buff, line_len, copy_len);
-	if (char_pos)
-		return (-1);
-	return (line_len);
+	char_pos = ft_strchr(data->buff, '\n');
+	copy_len = (char_pos) ? (char_pos - data->buff) : rv;
+	data->line_len += copy_len;
+	buff_to_line(line, data, copy_len);
+	return ((char_pos != NULL));
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	static char	buff[BUFF_SIZE + 1];
-	int			rv;
-	int			line_len;
+	static t_data	data;
+	int				rv;
 
-	if ((line_len = prelim_checks(line, buff)) == -1)
+	if (!(line))
+		return (-1);
+	if (prelim_checks(line, &data))
 		return (1);
-	if (((rv = read(fd, buff, BUFF_SIZE)) == 0 && ft_strlen(*line) == 0)
-			|| rv == -1)
+	if (((rv = read(fd, data.buff, BUFF_SIZE)) == 0 && ft_strlen(*line) == 0)
+		|| rv == -1)
 		return (rv);
 	if (rv == 0 && ft_strlen(*line) > 0)
 		return (1);
 	while (rv)
 	{
-		if ((line_len = run_copy(line, buff, line_len, rv)) == -1
-				|| rv == 0)
+		if ((run_copy(line, &data, rv)) || rv == 0)
 			return (1);
-		if ((rv = read(fd, buff, BUFF_SIZE)) == -1)
+		if ((rv = read(fd, data.buff, BUFF_SIZE)) == -1)
 			return (-1);
 		if (rv == 0)
 			return (1);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 16:40:51 by rhallste          #+#    #+#             */
-/*   Updated: 2017/10/18 15:12:55 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/10/18 22:44:24 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # ifndef BUFF_SIZE
 #  define BUFF_SIZE	100
 # endif
+
+typedef struct	s_data
+{
+	int			line_len;
+	char		buff[BUFF_SIZE + 1];
+}				t_data;
 
 int	get_next_line(const int fd, char **line);
 
