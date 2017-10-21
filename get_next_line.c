87c4460e5b6e53fb9f77c64ba78e3b2105084c68
RJ Hallsted <rhallste@student.42.us.org>
2017-10-21 14:18:52 -0700

    Made code more clear around call to run_copy

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,19 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 16:38:28 by rhallste          #+#    #+#             */
-/*   Updated: 2017/10/21 13:58:03 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/10/21 14:17:34 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 ** Current things to change:
-** 1) run_copy shouldn't be necessary if data.buff is empty. Or it should fail,
-** **or somtthinig like that. The code should check for the 'fullness' of buff
-** **and handle correctly if it's empty
-** 2) add support for multiple file descriptors
+** 1) add support for multiple file descriptors
 */
 
 #include <unistd.h>
 #include <string.h>
 #include "libft/libft.h"
 #include "get_next_line.h"
-
-#include <stdio.h>
 
 /*
 ** shift_chars_left assumes shift_by < BUFF_SIZE
@@ -64,12 +59,13 @@ int			get_next_line(const int fd, char **line)
 	if (!(line))
 		return (-1);
 	*line = NULL;
-	if (run_copy(line, &data, ft_strlen(data.buff)))
+	if (ft_strlen(data.buff) > 0
+		&& run_copy(line, &data, ft_strlen(data.buff)))
 		return (1);
 	if ((rv = read(fd, data.buff, BUFF_SIZE)) == -1
-		|| (rv == 0 && ft_strlen(*line) == 0))
+		|| (rv == 0 && *line == NULL))
 		return (rv);
-	if (rv == 0 && ft_strlen(*line) > 0)
+	if (rv == 0 && *line != NULL)
 		return (1);
 	while (rv)
 	{
